762b66d97f92405b1700c9c6c47359276854acc4
matweyes <matweyes@gmail.com>
2026-07-28 07:33:05 +0000

    Add `kitchen` app with models, forms, and tests

---
+++ kitchen/forms.py
@@ -0,0 +1,98 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+from kitchen.models import Dish, Cook
+
+
+class DishForm(forms.ModelForm):
+    cooks = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Dish
+        fields = "__all__"
+
+
+class CookCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Cook
+        fields = UserCreationForm.Meta.fields + (
+            "years_of_experience",
+            "first_name",
+            "last_name",
+        )
+
+    def clean_years_of_experience(self):
+        return validate_years_of_experience(
+            self.cleaned_data["years_of_experience"]
+        )
+
+
+class CookExperienceUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Cook
+        fields = ["years_of_experience"]
+
+    def clean_years_of_experience(self):
+        return validate_years_of_experience(
+            self.cleaned_data["years_of_experience"]
+        )
+
+
+def validate_years_of_experience(years_of_experience):
+    if not isinstance(years_of_experience, int):
+        raise ValidationError(
+            "Years of experience should be an integer"
+        )
+    if years_of_experience < 0:
+        raise ValidationError(
+            "Years of experience cannot be negative"
+        )
+    if years_of_experience > 80:
+        raise ValidationError(
+            "Years of experience cannot exceed 80"
+        )
+    return years_of_experience
+
+
+class CookSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by username"
+            }
+        ),
+    )
+
+
+class DishSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name"
+            }
+        ),
+    )
+
+
+class DishTypeSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name"
+            }
+        ),
+    )
